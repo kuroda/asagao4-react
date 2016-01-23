ea750d5bbf7a08a3318eccd700fa9c05d843abe0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-01-23 17:22:06 +0900

    記事編集フォーム: 作り始め

--- app/assets/javascripts/components/article_form.es6.jsx
+++ app/assets/javascripts/components/article_form.es6.jsx
@@ -1,5 +1,20 @@
 class ArticleForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = props
+  }
+
   render() {
-    return <div />;
+    return (
+      <form>
+        <table className='attr'>
+          <tr>
+            <th><label htmlFor='article_title'>タイトル</label></th>
+            <td><input size='20' type='text' name='article[title]'
+              value={this.state.title} /></td>
+          </tr>
+        </table>
+      </form>
+    )
   }
 }

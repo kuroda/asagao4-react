7e92c0e88ffb15294730dd3cde3ac278d99c2fee
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-01-23 17:25:23 +0900

    記事編集フォーム: error/warning の除去

--- app/assets/javascripts/components/article_form.es6.jsx
+++ app/assets/javascripts/components/article_form.es6.jsx
@@ -8,11 +8,13 @@ class ArticleForm extends React.Component {
     return (
       <form>
         <table className='attr'>
-          <tr>
-            <th><label htmlFor='article_title'>タイトル</label></th>
-            <td><input size='20' type='text' name='article[title]'
-              value={this.state.title} /></td>
-          </tr>
+          <tbody>
+            <tr>
+              <th><label htmlFor='article_title'>タイトル</label></th>
+              <td><input size='20' type='text' name='article[title]'
+                defaultValue={this.props.title} /></td>
+            </tr>
+          </tbody>
         </table>
       </form>
     )

e8829750ebd1f4581edddb1862fe23c5fc5349c4
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-01-25 23:54:38 +0900

    Add _method and authenticity_token to the form

--- app/assets/javascripts/components/article_form.es6.jsx
+++ app/assets/javascripts/components/article_form.es6.jsx
@@ -5,8 +5,13 @@ class ArticleForm extends React.Component {
   }
 
   render() {
+    let csrfToken = $('meta[name=csrf-token]').attr('content')
     return (
-      <form>
+      <form action='/admin/articles/10' method='post'
+        accept-charset='UTF-8'>
+        <input type='hidden' name='_method' value='patch' />
+        <input type='hidden' name='utf8' value='✓' />
+        <input type='hidden' name='authenticity_token' value={csrfToken} />
         <table className='attr'>
           <tbody>
             <tr>
@@ -29,12 +34,26 @@ class ArticleForm extends React.Component {
             <tr>
               <th><label htmlFor='article_expired_at_1i'>掲載終了日時</label></th>
               <td>
+                <input type='hidden' name='article[no_expiration]' value='0' />
+                <input type='checkbox' name='article[no_expiration]' value='1'
+                  id='article_member_only' checked={this.props.no_expiration} />
+                <label htmlFor='article_no_expiration'>掲載終了日を設定しない</label>
                 <DatetimeSelect objectName='article' attrName='expired_at'
                   startYear='2000' defaultValue={this.props.expired_at} />
               </td>
             </tr>
+            <tr>
+              <th><label>会員限定</label></th>
+              <td>
+                <input type='hidden' name='article[member_only]' value='0' />
+                <input type='checkbox' name='article[member_only]' value='1'
+                  id='article_member_only' checked={this.props.member_only} />
+                <label htmlFor='article_member_only'>会員限定</label>
+              </td>
+            </tr>
           </tbody>
         </table>
+        <p><input type='submit' name='commit' value='更新する' /></p>
       </form>
     )
   }
